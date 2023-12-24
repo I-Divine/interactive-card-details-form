04113589a5d907da2af056c7b62f5c37cd2cab3f
I-Divine <igwedinmaifechukwu@gmail.com>
2023-12-24 08:27:49 +0100

    Added styling

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,40 @@
+import style from "../styles/login.module.css";
+import { useState } from "react";
 const Login = () => {
+  const [nameError, setNError] = useState(true);
+  const [cNumError, setNumError] = useState(true);
+  const [dateError, setDateError] = useState(true);
+  const [cvcError, setCvcError] = useState(true);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (Number(document.getElementById("cardNum").value) == NaN) {
+      console.log("true");
+    }
+  };
   return (
     <>
-      <form>
+      <form className={style.login} onSubmit={handleSubmit}>
         <section>
           <label htmlFor="holderName">cardholder name </label>
+          <br />
           <input
             type="text"
             id="holderName"
             name="holderName"
             placeholder="e.g. Jane Appleseed"
           />
+          {nameError == true ? (
+            <>
+              <br />
+              <strong className={style.error}>Can't be blank</strong>
+            </>
+          ) : (
+            <></>
+          )}
         </section>
         <section>
           <label htmlFor="cardNum">card number</label>
+          <br />
           <input
             type="tel"
             name="cardNum"
@@ -20,18 +42,32 @@ const Login = () => {
             placeholder="e.g. 1234 5678 9123 0000"
           />
         </section>
-        <section style={{ display: "flex" }}>
+        <section style={{ display: "grid", gridTemplateColumns: "1fr 1fr" }}>
           <div>
             <label htmlFor="date">Exp. date (mm/yy)</label>
-            <input type="number" id="dateMonth" name="date" placeholder="MM" />
-            <input type="number" id="dateYear" name="date" placeholder="YY" />
+            <br />
+            <input
+              style={{ width: "30%" }}
+              type="number"
+              id="dateMonth"
+              name="date"
+              placeholder="MM"
+            />
+            <input
+              style={{ width: "30%" }}
+              type="number"
+              id="dateYear"
+              name="date"
+              placeholder="YY"
+            />
           </div>
           <div>
             <label htmlFor="CVC">cvc</label>
+            <br />
             <input type="number" name="CVC" id="cvc" placeholder="e.g. 123" />
           </div>
         </section>
-        <button type="submit">Submit</button>
+        <button type="submit">Confirm</button>
       </form>
     </>
   );
