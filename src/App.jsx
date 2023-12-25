d7e19504ec24e8d12cf63424ead55788b26580d8
I-Divine <igwedinmaifechukwu@gmail.com>
2023-12-26 00:21:44 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import { useState } from "react";
 import Login from "./components/Login";
 import Card_BG from "./components/Card_BG";
 // import "./App.css";
+import style from "./styles/app.module.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className={style.app}>
       <Card_BG />
       <Login />
-    </>
+    </div>
   );
 }
 
